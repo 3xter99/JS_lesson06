1b6405367b95249498c7d5844bee483550afeabc
3xter <3x-ter@mail.ru>
2020-08-14 18:45:27 +0300

    Did game

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-
+//
 // const isNumber = function(n) {
 //     return !isNaN(parseFloat(n)) && isFinite(n)
 // }
@@ -26,43 +26,54 @@
 //  return getResponse()
 // }
 // random()
-//
+
+
+function randomInteger(min, max) {
+    let rand = min - 0.5 + Math.random() * (max - min + 1);
+    return Math.round(rand);
+}
 
 
 const isNumber = function(n) {
     return !isNaN(parseFloat(n)) && isFinite(n)
 }
 
-function random(numberAttempts, initialNumberAttempts) {
+function random() {
+    let hiddenNumber = randomInteger(0, 100);
+    console.log(hiddenNumber);
 
-    let hiddenNumber = 10;
 
-    function getResponse() {
+    function getResponse(numberAttempts, initialNumberAttempts) {
+
         let answer = prompt('"Угадай число от 1 до 100"')
         initialNumberAttempts = initialNumberAttempts || numberAttempts
         if (numberAttempts === 1) {
             if (!confirm('Попытки закончились, хотите сыграть еще?')) {
                 return alert('До свидания')
             } else {
-                return random(initialNumberAttempts)
+                return getResponse(initialNumberAttempts)
             }
         }
         if (answer > hiddenNumber) {
-            alert(`Загаданное число меньше, осталось ${numberAttempts-1} попыток`)
-            random(numberAttempts-1, initialNumberAttempts)
-        } else if ( answer == null) {
+            alert(`Загаданное число меньше, осталось ${numberAttempts - 1} попыток`)
+            getResponse(numberAttempts - 1, initialNumberAttempts)
+        } else if (answer == null) {
             return alert('Выходим')
-        } if (!isNumber(answer)) {
-            alert('Введите число')
-            random(numberAttempts, initialNumberAttempts)
-        } else if (answer < hiddenNumber) {
-            alert(`Загаданное число больше, осталось ${numberAttempts-1} попыток`)
-            random(numberAttempts - 1, initialNumberAttempts)
-        } else if ( +answer === hiddenNumber) {
-            alert('Победа, ты угадал, это число ' + hiddenNumber)
         }
-
+        if (!isNumber(answer)) {
+            alert('Введите число')
+            getResponse(numberAttempts, initialNumberAttempts)
+        } else if (answer < hiddenNumber) {
+            alert(`Загаданное число больше, осталось ${numberAttempts - 1} попыток`)
+            getResponse(numberAttempts - 1, initialNumberAttempts)
+        } else if (+answer === hiddenNumber) {
+            if (!confirm('Попытки закончились, хотите сыграть еще?')) {
+                return alert('До свидания')
+            } else {
+                return random()
+            }
+        }
     }
-    return getResponse()
+    return getResponse(10)
 }
-random(10)
+random()
