939576b04e72813425c5cef8077544766a4d4c2a
3xter <3x-ter@mail.ru>
2020-08-14 09:15:48 +0300

    Did game

---
+++ app.js
@@ -0,0 +1,28 @@
+const isNumber = function(n) {
+    return !isNaN(parseFloat(n)) && isFinite(n)
+}
+
+function random() {
+    let x = 10;
+    let y = prompt('"Угадай число от 1 до 100"')
+    function getResponse() {
+        if (y > x) {
+            alert('Загаданное число меньше')
+            random()
+        } else if ( y == null) {
+            return alert('Выходим')
+        } else if (y < x && y > 0 || y < 0) {
+            alert('Загаданное число больше')
+            random()
+        } else if (!isNumber(y)){
+            alert('Введите число')
+            random()
+        } else if ( +y === x) {
+            alert('Победа, ты угадал, это число ' + x)
+        }
+
+    }
+ return getResponse()
+}
+random()
+
